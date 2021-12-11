6df51b2d5716f5023bc5ee659eed5cc3d9b80887
Ryan <tamielryan@gmail.com>
2021-12-12 00:16:53 +0900

    Initial Commit

---
+++ SwiftFormValidator/SwiftFormValidatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftFormValidatorApp.swift
+//  SwiftFormValidator
+//
+//  Created by Ryan J. W. Kim on 2021/12/12.
+//
+
+import SwiftUI
+
+@main
+struct SwiftFormValidatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
