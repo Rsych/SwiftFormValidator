72f1796ecca4c16da72e7cee283fdfdacacf49c8
Ryan <tamielryan@gmail.com>
2021-12-12 15:35:26 +0900

    FormState

---
+++ SwiftFormValidator/Form/Event/FormState.swift
@@ -0,0 +1,36 @@
+//
+//  FormState.swift
+//  SwiftFormValidator
+//
+//  Created by Ryan J. W. Kim on 2021/12/12.
+//
+
+import Foundation
+
+struct User: Equatable {
+    let firstName: String
+    let lastName: String
+    let email: String
+    let dob: Date
+}
+
+enum FormState {
+    case valid(user: User)
+    case fail(error: ValidationError)
+}
+
+extension FormState: Equatable {
+    
+    static func == (lhs: FormState, rhs: FormState) -> Bool {
+        switch (lhs, rhs) {
+        case (.valid(user: let lhsType),
+              .valid(user: let rhsType)):
+            return lhsType == rhsType
+        case (.fail(error: let lhsType),
+              .fail(error: let rhsType)):
+            return lhsType.errorDescription == rhsType.errorDescription
+        default:
+            return false
+        }
+    }
+}
